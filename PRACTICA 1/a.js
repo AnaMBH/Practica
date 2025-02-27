0777efc8ac29c5a00c0bc642979bca4cc43cf9c9
Ana <165413489+AnaMBH@users.noreply.github.com>
2025-02-27 19:57:29 +0100

    Actualización 27-02

---
+++ PRACTICA 1/a.js
@@ -0,0 +1,151 @@
+const moviesPopular = [
+    
+    {
+        image: "img/la_sustancia.jpg",
+        title: "The substance",
+        genre: "Fiction",
+        description: "You, but better in every way'. That's the promise, a revolutionary product based on cell division, creating a younger, more beautiful, more perfect alter ego.",
+        rating: "6.9",
+        age: "2024"
+    },
+    {
+        image: "img/deadpool_and_wolverine.jpg",
+        title: "Deadpool & Wolverine",
+        genre: "Action",
+        description: "An apathetic Wade Wilson strives for civilian life after leaving behind his days as Deadpool, a morally flexible mercenary. But when his home world faces an existential threat, Wade must reluctantly re-clothe himself with an even more reluctant Wolverine.",
+        rating: "6.7",
+        age: "2024"
+    },
+    {
+        image: "img/mufasa.jpg",
+        title: "Mufasa: The lion king",
+        genre: "Animation",
+        description: "Rafiki, Timon and Pumba tell Kiara the story of Mufasa, her grandfather, through flashbacks. The story reveals that Mufasa was an orphaned cub until he met Taka, a kindly lion and heir to a royal line. Together, they embark on an adventure with a group of misfits in search of their destiny, facing a dangerous enemy that will test their bonds.",
+        rating: "6.3",
+        age: "2024"
+    },
+    {
+        image: "img/super-mario-bros.jpg",
+        title: "The Super Mario Bros. Movie",
+        genre: "Animation",
+        description: "While working on an underground malfunction, Brooklyn plumbers Mario and his brother Luigi travel through a mysterious pipe to a magical new world. But when the brothers get separated, Mario must embark on an epic quest to find Luigi. With the help of the local mushroom Toad and a few combat notions from the Mushroom Kingdom's warrior leader, Princess Peach, Mario discovers the power within himself.",
+        rating: "6.6",
+        age: "2024"
+    }
+]
+
+const moviesRated = [
+    
+    {
+        image: "img/oppenheimer.jpg",
+        title: "Oppenheimer",
+        genre: "Drama",
+        description: "In wartime, the brilliant American physicist Julius Robert Oppenheimer, at the head of the ‘Manhattan Project’, leads nuclear tests to build the atomic bomb for his country. Shocked by its destructive power, Oppenheimer questions the moral consequences of his creation. From then on, and for the rest of his life, he would firmly oppose the use of nuclear weapons. ",
+        rating: "7.4",
+        age: "2025"
+    },
+    {
+        image: "img/la_infiltrada.jpg",
+        title: "La infiltrada",
+        genre: "Thriller",
+        description: "Based on the true story of Aranzazu Berradre Marín, a National Police agent who infiltrated ETA for 8 years. At the age of 20, she became the only woman to live with ETA leaders to dismantle a key commando. During her mission, she cut all family ties, risking her life to prevent an attack when the terrorist group declared a false truce. The story of a brave woman who changed her life to save others.",
+        rating: "7.2", 
+        age: "2024"
+    },
+    {
+        image: "img/dune.jpg",
+        title: "Dune: Part two",
+        genre: "Fiction",
+        description: "After the events of the first part on the planet Arrakis, young Paul Atreides joins the Fremen tribe and begins a spiritual and martial journey to become a messiah, while trying to avoid the horrible but inevitable future he has witnessed: a Holy War in his name, which spreads across the known universe...",
+        rating: "7.7",
+        age: "2024"
+    },
+    {
+        image: "img/attack-on-titan.jpg",
+        title: "Attack on Titan: The Last Attack",
+        genre: "Animation",
+        description: "Eren Yeager lives in a city that is protected by giant walls that protect its citizens from the Titans, huge creatures whose sole purpose is to devour people. However, things take an unexpected turn when two titans, the Colossal and the Battleship, break through the walls and enter the city. Eren's mother is eaten in front of him, a moment that makes him decide that he will extinguish all the titans in the world no matter what the cost.",
+        rating: "7.7",
+        age: "2025"
+    },
+    {
+        image: "img/spider-man-no-way-home.jpg",
+        title: "Spider-Man: No Way Home",
+        genre: "Fantasy",
+        description: "For the first time in Spider-Man's cinematic history, our hero, neighbour and friend is unmasked, and thus no longer able to separate his normal life from the enormous risks that come with being a superhero. When he asks Doctor Strange for help, the risks become even more dangerous, forcing him to discover what it really means to be him. Sequel to ‘Spider-Man: Far From Home’.",
+        rating: "7.0",
+        age: "2021"
+    }
+]
+
+const moviesSoon = [
+    
+    {
+        image: "img/attack-on-titan.jpg",
+        title: "Attack on Titan: The Last Attack",
+        genre: "Animation",
+        description: "Eren Yeager lives in a city that is protected by giant walls that protect its citizens from the Titans, huge creatures whose sole purpose is to devour people. However, things take an unexpected turn when two titans, the Colossal and the Battleship, break through the walls and enter the city. Eren's mother is eaten in front of him, a moment that makes him decide that he will extinguish all the titans in the world no matter what the cost.",
+        rating: "7.7",
+        age: "2025"
+    },
+    {
+        image: "img/bravenewworld.jpg",
+        title: "Captain America: Brave New World",
+        genre: "Action",
+        description: "After meeting with newly elected US President Thaddeus Ross (Harrison Ford), Sam finds himself in the middle of an international incident. He must uncover the reason behind a nefarious global plot before the true mastermind has the entire world seeing red.",
+        rating: '<i class="fa-solid fa-hourglass-start"></i>',
+        age: "2025"
+    },
+    {
+        image: "img/flight_risk.jpg",
+        title: "Flight Risk",
+        genre: "Thriller",
+        description: "A pilot (Mark Wahlberg) transports a lieutenant general (Michelle Dockery) in his small plane, who is guarding a witness (Topher Grace) who is to testify in a mob trial. As they cross the Alaskan mountains, tensions run high, as not everyone on board is who they appear to be. And at 10,000 feet there is no escape.",
+        rating: '<i class="fa-solid fa-hourglass-start"></i>',
+        age: "2025"
+    },
+    {
+        image: "img/sinners.jpg",
+        title: "Sinners",
+        genre: "Terror",
+        description: "Trying to put their troubled lives behind them, twin brothers (Jordan) return to their hometown for a fresh start, only to discover that an even greater evil is waiting to greet them.",
+        rating: '<i class="fa-solid fa-hourglass-start"></i>',
+        age: "2025"
+    }
+]
+
+const moviesBillboard = [
+   
+    {
+        image: "img/moana_2.jpg",
+        title: "Moana 2",
+        genre: "Animation",
+        description: "After receiving an unexpected call from her ancestors, Vaiana must travel to the far seas of Oceania and into dangerous lost waters for an unprecedented adventure.",
+        rating: "5.7",
+        age: "2024"
+    },
+    {
+        image: "img/oppenheimer.jpg",
+        title: "Oppenheimer",
+        genre: "Drama",
+        description: "In wartime, the brilliant American physicist Julius Robert Oppenheimer, at the head of the ‘Manhattan Project’, leads nuclear tests to build the atomic bomb for his country. Shocked by its destructive power, Oppenheimer questions the moral consequences of his creation. From then on, and for the rest of his life, he would firmly oppose the use of nuclear weapons. ",
+        rating: "7.4",
+        age: "2025"
+    },
+    {
+        image: "img/den_of_thieves_2_pantera.jpg",
+        title: "Den of thieves 2: Pantera",
+        genre: "Thriller",
+        description: "Donnie teams up with the Panther mob to pull off the biggest diamond heist ever seen. Meanwhile, Nick, the cop who has gone after him in the past, is broke and tired of being the hunter, so he proposes to join his gang and pull off the heist together. If this challenge succeeds, they will have pulled off the biggest heist in history.",
+        rating: "5.7",
+        age: "2025"
+    },
+    {
+        image: "img/mikaela.jpg",
+        title: "Mikaela",
+        genre: "Thriller",
+        description: "On the eve of Epiphany, an unprecedented snowstorm ravages Spain. Amidst the chaos of a collapsed motorway, a group of robbers seize the opportunity to hold up an armoured car. Just a few metres away is Leo (Antonio Resines), a cop on his last legs who has nothing to lose. With the unexpected help of a young woman (Natalia Azahara), they try to prevent the gang from escaping with the loot in a chase against the clock in the middle of the storm.",
+        rating: "5.1",
+        age: "2025"
+    }
+]    
